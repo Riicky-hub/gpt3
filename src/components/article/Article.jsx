28df301defaa0abeca31c60c5e05cda225efc39b
Riicky-hub <richardjosephace1960@gmail.com>
2022-12-16 00:54:08 -0300

    Blog finished

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './article.css';
 
-const Article = () => {
+const Article = (props) => {
   return (
-    <div>Article</div>
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={props.imgUrl} alt="Blog" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{props.date}</p>
+          <h3>{props.title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
   )
 }
 
